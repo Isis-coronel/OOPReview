2f6cc60581fe8227e184ac775ad4b0373a478402
Isis <isisd.coronel@gmail.com>
2020-07-14 12:28:52 +0200

    Icreated the mobiles to test it

--- mobiles.ts
+++ mobiles.ts
@@ -1,4 +1,4 @@
-import {Mobile} from "../mobile";
+import {Mobile} from "./mobile";
 
 let mobile1 = new Mobile("Nokia3210", "3210", "Nokia", 0, "blue", 0, 14 );
 let mobile2 = new Mobile("iPhone3G", "A1325", "Apple", 16, "white", 2, 90);
